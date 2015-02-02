57b211b7dbe2203812c9145f4458acb927e2150f
Ryan Sobol <contact@ryansobol.com>
2015-02-01 22:08:39 -0800

    Release v1.1.0

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ or
 
 The MIT License (MIT)
 
-Copyright (c) 2014 Ryan Sobol
+Copyright (c) 2015 Ryan Sobol
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
--- gitio.rb
+++ gitio.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Gitio < GithubGistFormula
-  url 'https://gist.githubusercontent.com/ryansobol/7214372/raw/b7f202c206c66413d2cf89173eb02f829315d2bd/gitio'
-  sha1 '8a51c53dc10989a0ae2cab6e500b731fa9edbfac'
-  version '1.0.1'
+  url 'https://gist.githubusercontent.com/ryansobol/7214372/raw/7ceabd14718cdf8f4c9cf8bd6a70daa4874006e5/gitio'
+  sha1 '23c702a6ca19f1892b178d1b128fe863735c3614'
+  version '1.1.0'
   homepage 'https://gist.github.com/ryansobol/7214372'
 end
