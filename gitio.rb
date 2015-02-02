39c88c398ea310c665c58b2ee4d78ae533928ffe
Ryan Sobol <contact@ryansobol.com>
2015-02-02 07:29:03 -0800

    Release v1.1.1

--- gitio.rb
+++ gitio.rb
@@ -1,8 +1,8 @@
 require 'formula'
 
 class Gitio < GithubGistFormula
-  url 'https://gist.githubusercontent.com/ryansobol/7214372/raw/7ceabd14718cdf8f4c9cf8bd6a70daa4874006e5/gitio'
-  sha1 '23c702a6ca19f1892b178d1b128fe863735c3614'
-  version '1.1.0'
+  url 'https://gist.githubusercontent.com/ryansobol/7214372/raw/001e801e2fbe652c8839ffc14aa59a9e338ae0db/gitio'
+  sha1 'f7bf68c2bd6a5d73c00bec178c0fba940bf0d820'
+  version '1.1.1'
   homepage 'https://gist.github.com/ryansobol/7214372'
 end
